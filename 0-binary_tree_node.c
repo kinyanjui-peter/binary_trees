0c88e83727ec869af88a68c07816ab9114d65c17
peter <01peterkinyanjui@gmail.com>
2023-10-31 18:56:40 -0700

    task0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,10 +9,8 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
-	/*check if node is available*/
-	if parent == NULL
-		return (NULL);
-	new = (binary_tree_t *)malloc(sizeof(binary_tree_t ));
+	
+	new = malloc(sizeof(binary_tree_t));
 	if(new == NULL)
 		return (NULL);
 	new->parent = parent;
